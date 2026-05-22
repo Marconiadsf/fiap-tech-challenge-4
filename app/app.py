5bd8aeb258bdea2613c56c2e79ecece9688c9c0f
Marconi Abou <Marconiadsf@users.noreply.github.com>
2026-05-22 19:30:26 -0300

    Update page title to Portuguese in app.py

--- app/app.py
+++ app/app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 
 st.set_page_config(
-    page_title="Obesity Predictor | Tech Challenge 4",
+    page_title="Preditoor de Tendência a Obesidade | Tech Challenge 4",
     page_icon="🏥",
     layout="wide",
     initial_sidebar_state="expanded",
